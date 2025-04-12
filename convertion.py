3c284fb77506750ffe3f14e3a98a755f079f5563
Dylan-wg <1942601676@qq.com>
2025-04-12 18:39:02 +0800

    UPDATE

--- convertion.py
+++ convertion.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image
 
 
 def convert(directory, old_extension, new_extension):
@@ -16,3 +17,82 @@ def convert(directory, old_extension, new_extension):
                 except Exception as e:
                     print(f'"{old_file_path}": {e}')
     print("Total convertion:", counter)
+
+
+def single_to_jpg(path, filename):
+    name = ""
+    for i in filename:
+        if i != ".":
+            name += i
+        else:
+            break
+    try:
+        with Image.open(path + filename) as img:
+            if img.mode in ('RGBA', 'LA'):
+                rgb_img = img.convert('RGB')
+            else:
+                rgb_img = img
+            rgb_img.save(path + name + ".jpg", 'JPEG', quality=100)
+            print(f"Done:", path + name + ".jpg")
+
+    except Exception as e:
+        print(f"Fail: {e}")
+        return 0
+    return 1
+
+
+def to_jpg(path, filename, **kwargs):
+    total = 0
+    if filename == ".":
+        for f in os.listdir(path):
+            if f.lower().endswith('.png'):
+                total += single_to_jpg(path, f)
+                if kwargs["delete"]:
+                    os.remove(os.path.join(path, f))
+        print(total, "processes completed.")
+    else:
+        single_to_jpg(path, filename)
+        if kwargs["delete"]:
+            os.remove(os.path.join(path, filename))
+
+
+def rename(path: str, name_format: str, var: str, begin: str, end: str):
+    total = 0
+    name_f, ext_f = os.path.splitext(name_format)
+
+    try:
+        begin = int(begin)
+        if end != "inf":
+            end = int(end)
+        use_ascii = False
+    except ValueError:
+        begin = ord(begin)
+        if end != "inf":
+            end = ord(end)
+        use_ascii = True
+    iter_var = begin
+
+    for filename in os.listdir(path):
+        old_path = os.path.join(path, filename)
+
+        if os.path.isfile(old_path):
+            name, ext = os.path.splitext(filename)
+
+            if ext == ext_f:
+                new_name = name_f.replace(var, chr(iter_var) if use_ascii else str(iter_var)) + ext_f
+                new_path = os.path.join(path, new_name)
+
+                try:
+                    os.rename(old_path, new_path)
+                    print(f"Success: {filename} → {new_name}")
+                except Exception as e:
+                    print(f"Fail {filename}: {e}")
+                    continue
+
+                iter_var += 1
+                total += 1
+                if end != "inf":
+                    if iter_var > end:
+                        break
+
+    print(total, "processes completed.")
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from download import *
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", choices=["download", "convert", "open"])
+    parser.add_argument("command", choices=["download", "convert", "open", "toJPG", "rename"])
     parser.add_argument('param', nargs="*")
 
     args = parser.parse_args()
@@ -17,13 +17,20 @@ def main():
         if args.command == "download":
             download(args.param[0])
         elif args.command == "convert":
-            convert(args.param[0], args.param[1], args.param[2])
+            convert("./", args.param[0], args.param[1])
         elif args.command == "open":
             current_dir = get_exe_dir()
             folder_path = os.path.join(current_dir, 'downloads')
             if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
             subprocess.Popen(['explorer', folder_path])
+        elif args.command == "toJPG":
+            if len(args.param) == 2:
+                to_jpg("./", args.param[0], delete=False)
+            else:
+                to_jpg("./", args.param[0], delete=(args.param[1] == "del"))
+        elif args.command == "rename":
+            rename("./", args.param[0], args.param[1], args.param[2], args.param[3])
     except IndexError:
         print('Fail to execute: Missing parameters')
 
